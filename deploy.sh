5b6c7df0c79a85aebf61addd7b6943f720e078ef
NotExpectedYet <online@notexpectedyet.com>
2021-07-16 14:26:17 +0100

    deploy.sh script

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd src/.vuepress/dist
+
+# if you are deploying to a custom domain
+echo 'docs.octofarm.net' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+git push -f git@github.com:octofarm/octofarm.github.io.git gh-pages
+
+cd -
